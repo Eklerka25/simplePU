7ba7c2c0d5b2a84d685dac424723c2b7d0362c8a
Eklerka25 <134164161+Eklerka25@users.noreply.github.com>
2025-03-15 10:32:33 +0100

    First commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using Byte       = unsigned char;
+using Word       = unsigned short;
+using Dword      = unsigned long;
+using Qword      = unsigned long long
+
+struct cpu {
+
+};
+
+int main() {
+
+}
